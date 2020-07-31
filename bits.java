ed39005975fb549951ba43f074b13480c0e12603
unknown <singh.dajinder13@gmail.com>
2020-07-31 15:11:44 +0530

    updated

--- bits.java
+++ bits.java
@@ -8,6 +8,10 @@ public class bits{
         // System.out.println(UniqueNumberInArray(arr));
 
         // System.out.println(isPowerOfTwo(4));
+
+
+        // System.out.println(offToOn(5, 2));
+        
     }
 
     public static int countAllSetBits(int num){
@@ -48,5 +52,11 @@ public class bits{
     //Leetcode : 231============================================
     public static boolean isPowerOfTwo(int n) {
         return n>0 && (n & (n - 1))==0;
+    }
+
+
+    public static int offToOn(int num, int k){
+        int mask = 1<<k;
+        return num|mask;
     }
 }
